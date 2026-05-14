d621fd28de09b82cd6e44f033f33372395505447
Karthik S V <ksv1844@gmail.com>
2026-05-14 23:07:21 +0530

    commit 9

--- loops.py
+++ loops.py
@@ -4,3 +4,13 @@ for i in (1,2,3,4,5) :
 # Using range function
 for item in range(5) :
     print(f"Round: {item}")
+
+# Using range starting from 1
+for item in range(1,10) :
+    print(f"Round: {item}")
+
+# For loop used in application use cases
+scores = [80, 50, 60, 75]
+total = 0
+for score in scores:
+    total += scores
